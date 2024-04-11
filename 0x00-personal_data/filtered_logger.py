9ca78f5c859fe1a809d1e4a4a1dc410c86313c0f
Neelam <neelam_669@yahoo.com>
2024-04-11 12:10:46 +0000

    Task 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module for handling Personal Data
+"""
+import re
+from typing import List
+import logging
+import mysql.connector
+import os
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ Returns a log message obfuscated """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
