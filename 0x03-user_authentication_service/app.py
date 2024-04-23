91d22e95ec8dff0a3a092ac322e52ac71e2cbf3f
Neelam <neelam_669@yahoo.com>
2024-04-23 13:45:31 +0000

    Task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -17,3 +17,30 @@ def index() -> str:
       - JSON payload
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """ POST /users
+    Registers new user with email and pswd in x-www-form-urlencoded request,
+    or finds if user already registered based on email
+    """
+    form_data = request.form
+
+    if "email" not in form_data:
+        return jsonify({"message": "email required"}), 400
+    elif "password" not in form_data:
+        return jsonify({"message": "password required"}), 400
+    else:
+
+        email = request.form.get("email")
+        pswd = request.form.get("password")
+
+        try:
+            new_user = AUTH.register_user(email, pswd)
+            return jsonify({
+                "email": new_user.email,
+                "message": "user created"
+            })
+        except ValueError:
+            return jsonify({"message": "email already registered"}), 400
